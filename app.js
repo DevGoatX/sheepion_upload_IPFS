e481d863a65c1151e25526a496bd25acb43e43fc
Henry Jin <jinming0106@outlook.com>
2022-08-01 06:03:49 +0800

    changed some logic

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ require('@babel/register');
 const {Op} = require('sequelize');
 const _ = require('lodash');
 const sharp = require('sharp');
+const mt = require('media-thumbnail');
+const ffmpeg = require('ffmpeg-static')
+const genThumbnail = require('simple-thumbnail')
+
 const {pinata, getContentUrl} = require('./services/pinata');
 const temp = require('temp');
 const {ipfsNamePrefix} = require('./config');
@@ -19,49 +23,106 @@ const TYPE_LEGENDARY  = 'legendary';
 const assetPath = __dirname + '/assets';
 const outputPath = __dirname + '/output';
 
-async function upload() {
-  await uploadFolder(`${assetPath}/common/`, TYPE_COMMON);
-  await uploadFolder(`${assetPath}/rare/`, TYPE_RARE);
-  await uploadFolder(`${assetPath}/super rare/`, TYPE_SUPER_RARE);
-  await uploadFolder(`${assetPath}/legendary/`, TYPE_LEGENDARY);
+
+async function uploadContent(contentPath, metaDataName, extension = 'jpg') {
+  console.log('------- uploading content path: ', contentPath);
+  const {IpfsHash: hash} = await pinata.pinFromFS(contentPath, {
+    pinataMetadata: {
+      name: `${metaDataName}.${extension}`,
+    }
+  });
+  return hash;
+}
+
+function isMovie(extension) {
+  if (extension == 'mp4') {
+    return true;
+  }
+
+  return false;
 }
 
 async function uploadFolder(folder, type) {
   const metaUriList = [];
   console.log('------- folder name: ', folder);
 
-  const imageFolder = folder + 'image/';
+  const contentFolder = folder + 'content/';
   const metadataFolder = folder + 'metadata/';
 
   // Asyncronize
-  fs.readdir(imageFolder, async (err, files) => {
+  fs.readdir(contentFolder, async (err, files) => {
+    let index = 0;
     for (let i = 0; i < files.length; i++) {
       const file = files[i];
-      const fileName = file.split('.')[0];
+      if (file == ".DS_Store")  continue;
 
+      const fileName = file.split('.')[0];
+      const copies = fileName.split('_')[1];
+      
+      const extension = file.split('.')[1];
+      
       // get original path name
-      const metaName = `${ipfsNamePrefix}_${type}_original_${i}`;
-      const imagePath = imageFolder + file;
-      const originalHash = await uploadImage(imagePath, metaName);
-      console.log(`------- ${type} ${i + 1}th image hash: ${originalHash}`);
+      const metaName = `${ipfsNamePrefix}_${type}_${index + 1}`;
+      const contentPath = contentFolder + file;
+      console.log(`----- content path: ${contentPath}`);
+
+      console.log(`----- ${type} ${index + 1}th uploading to IPFS`);
+      const originalHash = await uploadContent(contentPath, metaName, extension);
+      console.log(`------- ${type} ${index + 1}th hash: ${originalHash}`);
+      console.log(`------- ${type} ${index + 1}th url: ${getContentUrl(originalHash)}`);
 
       // get thumbnail path name
-      const thumMetaName = `${ipfsNamePrefix}_${type}_thmbnail_${i}`;
-      const thumbnailImage = sharp(imagePath).resize({width: 350, height: 350});
-      const thumbnailPath = temp.path() + '.png';
-      await thumbnailImage.toFile(thumbnailPath);
-      const thumbnailHash = await uploadImage(thumbnailPath, thumMetaName);
-      console.log(`------- ${type} ${i + 1}th thumbnail hash: ${thumbnailHash}`);
+      const thumMetaName = `${ipfsNamePrefix}_${type}_thmbnail_${index + 1}`;
+      let thumbnailPath = temp.path() + '.png';
+      let existThumbnail = true;
+
+      if (isMovie(extension)) { // video
+        try {
+          existThumbnail = await new Promise((resolve) => {
+            genThumbnail(contentPath, thumbnailPath, '350x350', {path: ffmpeg.path})
+              .then(() => {
+                console.log('---------- thumbnail creation success for video file: ');
+                resolve(true);
+              })
+              .catch(err => {
+                console.log('---------- thumbnail creation error for video file: ', err);
+                resolve(false);
+              })
+          })
+          
+        } catch (err) {
+          console.log('---------- thumbnail creation error for video file: ', err);
+        }
+
+      } else { // image
+        const thumbnailImage = sharp(contentPath).resize({width: 350, height: 350});
+        await thumbnailImage.toFile(thumbnailPath);
+      }
+
+      let thumbnailHash = "";
+      if (existThumbnail) {
+        console.log(`----- ${type} ${index + 1}th thumbnail uploading to IPFS`);
+        thumbnailHash = await uploadContent(thumbnailPath, thumMetaName);
+        console.log(`------- ${type} ${index + 1}th thumbnail hash: ${thumbnailHash}`);
+        console.log(`------- ${type} ${index + 1}th thumbnail url: ${getContentUrl(thumbnailHash)}`);
+      }
 
       // read metadata from file
       try {
         const jsonString = fs.readFileSync(metadataFolder + fileName + '.json');
         let metadata = JSON.parse(jsonString);
-        metadata.image = getContentUrl(thumbnailHash);
+        if (existThumbnail) {
+          metadata.image = getContentUrl(thumbnailHash);
+        }
+
         metadata.rarity_type = type;
         metadata.properties.files[0].uri = getContentUrl(originalHash);
 
-        const jsonMetaName = `${ipfsNamePrefix}_${type}_metadata_${i}.json`
+        if (isMovie(extension)) {
+          metadata.animation_url = getContentUrl(originalHash);
+        }
+
+        const jsonMetaName = `${ipfsNamePrefix}_${type}_metadata_${index + 1}.json`
   
         // upload json file to ipfs
         const {IpfsHash: metadataHash} = await pinata.pinJSONToIPFS(metadata, {
@@ -70,35 +131,16 @@ async function uploadFolder(folder, type) {
           }
         });
   
-        console.log(`------- ${type} ${i + 1}th metadata hash: ${metadataHash}`);
-        metaUriList.push({metadataHash: metadataHash, thumbnailHash: thumbnailHash});
+        console.log(`------- ${type} ${index + 1}th metadata hash: ${metadataHash}`);
+        console.log(`------- ${type} ${index + 1}th metadata url: ${getContentUrl(metadataHash)}`);
+        metaUriList.push({metadataHash: metadataHash, thumbnailHash: thumbnailHash, copies: copies});
 
       } catch (err) {
-        console.log(`------ reading error of ${metadataFolder + fileName}.json --------`);
+        console.log(`------ reading error of ${metadataFolder + fileName}.json: `, err);
         continue;
       }
-    
-      // const metadata = {
-      //   name: `#${i + 1}`,
-      //   description: '',
-      //   external_url: getContentUrl(originalHash),
-      //   rarity_type: type,
-      //   attributes: [
-      //   ],
-      // };
-      // console.log(`------- ${type} ${i + 1}th metadata: `, metadata);
 
-      // const jsonMetaName = `${ipfsNamePrefix}_${type}_metadata_${i}.json`
-
-      // // upload json file to ipfs
-      // const {IpfsHash: metadataHash} = await pinata.pinJSONToIPFS(metadata, {
-      //   pinataMetadata: {
-      //     name: jsonMetaName,
-      //   }
-      // });
-
-      // console.log(`------- ${type} ${i + 1}th metadata hash: ${metadataHash}`);
-      // metaUriList.push({metadataHash: metadataHash, thumbnailHash: thumbnailHash});
+      index++;
     }
 
     const outputName = `${outputPath}/${type}.json`;
@@ -111,20 +153,82 @@ async function uploadFolder(folder, type) {
       }
     });
   })
+
+
+  // Syncronize
+  // await new Promise((resolve) => {
+  //   fs.readdir(contentFolder, async (err, files) => {
+  //     for (let i = 0; i < files.length; i++) {
+  //       const file = files[i];
+  //       const fileName = file.split('.')[0];
+  
+  //       // get original path name
+  //       const metaName = `${ipfsNamePrefix}_${type}_original_${index + 1}`;
+  //       const contentPath = contentFolder + file;
+  //       const originalHash = await uploadContent(contentPath, metaName);
+  //       console.log(`------- ${type} ${index + 1}th image hash: ${originalHash}`);
+  //       console.log(`------- ${type} ${index + 1}th image url: ${getContentUrl(originalHash)}`);
+  
+  //       // get thumbnail path name
+  //       const thumMetaName = `${ipfsNamePrefix}_${type}_thmbnail_${index + 1}`;
+  //       const thumbnailImage = sharp(contentPath).resize({width: 350, height: 350});
+  //       const thumbnailPath = temp.path() + '.png';
+  //       await thumbnailImage.toFile(thumbnailPath);
+  //       const thumbnailHash = await uploadContent(thumbnailPath, thumMetaName);
+  //       console.log(`------- ${type} ${index + 1}th thumbnail hash: ${thumbnailHash}`);
+  //       console.log(`------- ${type} ${index + 1}th thumbnail url: ${getContentUrl(thumbnailHash)}`);
+  
+  //       // read metadata from file
+  //       try {
+  //         const jsonString = fs.readFileSync(metadataFolder + fileName + '.json');
+  //         let metadata = JSON.parse(jsonString);
+  //         metadata.image = getContentUrl(thumbnailHash);
+  //         metadata.rarity_type = type;
+  //         metadata.properties.files[0].uri = getContentUrl(originalHash);
+  
+  //         const jsonMetaName = `${ipfsNamePrefix}_${type}_metadata_${index + 1}.json`
+    
+  //         // upload json file to ipfs
+  //         const {IpfsHash: metadataHash} = await pinata.pinJSONToIPFS(metadata, {
+  //           pinataMetadata: {
+  //             name: jsonMetaName,
+  //           }
+  //         });
+    
+  //         console.log(`------- ${type} ${index + 1}th metadata hash: ${metadataHash}`);
+  //         console.log(`------- ${type} ${index + 1}th metadata url: ${getContentUrl(metadataHash)}`);
+  //         metaUriList.push({metadataHash: metadataHash, thumbnailHash: thumbnailHash});
+  
+  //       } catch (err) {
+  //         console.log(`------ reading error of ${metadataFolder + fileName}.json --------`);
+  //         continue;
+  //       }
+  //     }
+  
+  //     const outputName = `${outputPath}/${type}.json`;
+  //     const jsonData = JSON.stringify(metaUriList);
+  //     await new Promise((writeResolve) => {
+  //       fs.writeFile(outputName, jsonData, 'utf-8', (err, data) => {
+  //         if (err) {
+  //           console.log('------ file write error: ', err);
+  //         } else {
+  //           console.log('------ file write success: ', data);
+  //         }
+  //       });
+  //       writeResolve(0);
+  //     })
+
+  //     resolve(0);
+  //   })
+  // })
 }
 
-async function uploadImage(imagePath, metaDataName) {
-  console.log('--------- image path: ', imagePath);
-  const {IpfsHash: imageHash} = await pinata.pinFromFS(imagePath, {
-    pinataMetadata: {
-      name: `${metaDataName}.jpg`,
-    }
-  });
-
-  console.log('---------- image hash: ', imageHash);
-  return imageHash;
+async function upload() {
+  await uploadFolder(`${assetPath}/common/`, TYPE_COMMON);
+  await uploadFolder(`${assetPath}/rare/`, TYPE_RARE);
+  await uploadFolder(`${assetPath}/super rare/`, TYPE_SUPER_RARE);
+  await uploadFolder(`${assetPath}/legendary/`, TYPE_LEGENDARY);
 }
-
 
 // load temp
 temp.track();
--- config/index.js
+++ config/index.js
@@ -4,9 +4,12 @@ const isTestNet = process.env.NODE_ENV !== 'production';
 const Config = {
   ipfsNamePrefix: isTestNet ? 'sheepion_test': 'sheepion',
   pinata: {
-    gateway: 'https://sheepion.mypinata.cloud',
-    apiKey: 'd08715be3716ef4440aa',
-    secret: '987455caf7d16375e741eafd01916f671f6df2ceea4c97b4ed2a5c4904d116a3',
+    // gateway: 'https://sheepion.mypinata.cloud',
+    // apiKey: 'd08715be3716ef4440aa',
+    // secret: '987455caf7d16375e741eafd01916f671f6df2ceea4c97b4ed2a5c4904d116a3',
+    gateway: 'https://gateway.pinata.cloud',
+    apiKey: 'aaf0eccc10e10e38c75f',
+    secret: '1a5aa6860fb27fcb11b646aa5d95e849c51f4de3c2c6120914e389f07b36c66e',
   },
   thumbnailSize: {
     width: 350,
